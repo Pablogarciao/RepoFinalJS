7a5ffba0b1b3e122f2a8f1ec589aabb7fa122a3c
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2023-10-27 15:23:56 -0500

    Agregando ast_test

---
+++ test/ast_test.test.ts
@@ -0,0 +1,31 @@
+import { Program, LetStatement, ReturnStatement, Identifier } from '../lpp/ast';
+import { Token, TokenType } from '../lpp/tokens';
+
+describe('AST', () => {
+  test('should create a LetStatement', () => {
+    const program = new Program([
+      new LetStatement(
+        new Token(TokenType.LET, 'variable'),
+        new Identifier(new Token(TokenType.IDENT, 'mi_var'), 'mi_var'),
+        new Identifier(new Token(TokenType.IDENT, 'otra_var'), 'otra_var')
+      ),
+    ]);
+
+    const programStr = program.toString();
+
+    expect(programStr).toBe('variable mi_var = otra_var;');
+  });
+
+  test('should create a ReturnStatement', () => {
+    const program = new Program([
+      new ReturnStatement(
+        new Token(TokenType.RETURN, 'regresa'),
+        new Identifier(new Token(TokenType.IDENT, 'mi_var'), 'mi_var')
+      ),
+    ]);
+
+    const programStr = program.toString();
+
+    expect(programStr).toBe('regresa mi_var;');
+  });
+});
